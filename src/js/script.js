1461bf60cb41aaa455ee739bcf8b9990b7323ade
Kir Neboula <newbie13xd@outlook.com>
2018-04-16 23:21:45 +0200

    rubbish

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let canvas = document.createElement("canvas"),
+    canvasContext = canvas.getContext("2d");
+
+image.onload = function () {
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    canvasContext.drawImage(image, 0, 0, image.width, image.height);
+
+    let dataURL = canvas.toDataURL();
+
+    document.write(dataURL);
+
+};
+
+image.src = url;
