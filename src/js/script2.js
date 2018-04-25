904b1e951bfd7008e6013a1de3e2a8108dcd0068
Newbyte <stehan253@edu.linkoping.se>
2018-04-25 11:05:14 +0000

    removed reminder I set for myself

--- src/js/script2.js
+++ src/js/script2.js
@@ -1,7 +1,5 @@
 "use strict";
 
-alert("Fråga om Berzan-färg");
-
 Date.prototype.getWeek = function() {
     let onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
