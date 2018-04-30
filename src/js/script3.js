ca30dec87c68d9ae370aa161cb590ece3cf5e47d
Kir Neboula <newbie13xd@outlook.com>
2018-04-30 15:28:26 +0200

    Schedule works again, now with enter as an option for confirmation

--- src/js/script3.js
+++ src/js/script3.js
@@ -1,13 +1,14 @@
 "use strict";
 
 Date.prototype.getWeek = function() {
-    let onejan = new Date(this.getFullYear(), 0, 1);
+    const onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 };
 
-let navigationButtons = document.getElementsByClassName("navButton");
-let navigationButtonsLength = navigationButtons.length;
-let contentIframe = document.getElementById("contentIframe");
+const navigationButtons = document.getElementsByClassName("navButton");
+const navigationButtonsLength = navigationButtons.length;
+const contentIframe = document.getElementById("contentIframe");
+const date = new Date();
 
 for (let i = 0; i < navigationButtonsLength; i++) {
     navigationButtons[i].addEventListener("click", () => {
@@ -18,7 +19,7 @@ for (let i = 0; i < navigationButtonsLength; i++) {
 function loadPage(page = 0) {
     switch (page) {
         case 0:
-            contentIframe.src = "html/schedule.html";
+            loadSchedulePage();
             break;
         case 1:
             contentIframe.src = "html/lunch.html";
@@ -29,6 +30,52 @@ function loadPage(page = 0) {
         case 3:
             contentIframe.src = "html/settings.html";
             break;
+    }
+}
+
+function loadSchedulePage() {
+    contentIframe.src = "html/schedule.html";
+    contentIframe.onload = () => {
+        const iframeDocument = contentIframe.contentDocument || contentIframe.contentWindow.document;
+        const inputFields = iframeDocument.getElementsByClassName("inputField");
+        const searchButton = iframeDocument.getElementById("searchClass");
+
+        searchButton.addEventListener("click", () => {
+            viewSchedule(true);
+        });
+
+        for (let i = 0; i < inputFields.length; i++) {
+            inputFields[i].addEventListener("keydown", (event) => {
+                const keyName = event.key;
+
+                if (keyName === "Enter") {
+                    viewSchedule(true);
+                }
+            })
+        }
+    };
+}
+
+function viewSchedule(clickInit = false) {
+    const iframeDocument = contentIframe.contentDocument || contentIframe.contentWindow.document;
+    const weekInputField = iframeDocument.getElementById("weekNumberField");
+    const classInputField = iframeDocument.getElementById("classNameField");
+    const schedule = iframeDocument.getElementById("schedule");
+    let currentWeek;
+    let className;
+
+    try {
+        currentWeek = weekInputField.value;
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (currentWeek === "") currentWeek = date.getWeek();
+    if (clickInit) className = classInputField.value;
+
+    schedule.src = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=89920/sv-se&type=-1&id=" + className + "&period=&week=" + currentWeek + "&mode=0&printer=0&colors=32&head=0&clock=0&foot=0&day=0&width=921&height=872&maxwidth=921&maxheight=872";
+    schedule.onload = () => {
+        contentIframe.height = (contentIframe.contentWindow.document.body.scrollHeight + 4) + "px";
     }
 }
 
