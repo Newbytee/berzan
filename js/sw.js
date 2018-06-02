abf986fb55272e59be10c8ed493cefecc0fe8323
Kir Neboula <newbie13xd@outlook.com>
2018-06-02 16:25:53 +0200

    Meep

--- js/sw.js
+++ js/sw.js
@@ -1,44 +1,17 @@
-"use strict";
+if ('serviceWorker' in navigator) {
 
-const cacheName = "v1";
-const cacheFiles = [
-    "../index.html",
-    "../html/about.html",
-    "../html/browsers.html",
-    "../html/etc.html",
-    "../html/lunch.html",
-    "../html/schedule.html",
-    "../html/settings.html",
-    "../html/script3.js",
-    "../css/reset.css",
-    "../css/style.css",
-    "../manifest.json"
-];
+    navigator.serviceWorker.register('js/worker.js').then(function(registration) {
+        console.log('Service worker registration succeeded: ', registration);
+    }).catch(function(error) {
+        console.log('Service worker registration failed: ', error);
+    });
 
-self.addEventListener("install", function (e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log("[Service Worker] Caching files ...");
-            return cache.addAll(cacheFiles);
-        })
-    );
-    console.log("... done");
-});
+} else {
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        // Get all the cache keys (cacheName)
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(thisCacheName) {
+    console.log('Service workers are not supported');
 
-                // If a cached item is saved under a previous cacheName
-                if (thisCacheName !== cacheName) {
+}
 
-                    // Delete that cached file
-                    console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-                    return caches.delete(thisCacheName);
-                }
-            }));
-        })
-    );
+self.addEventListener('fetch', function(e){
+
 });
