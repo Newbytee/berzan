f18bef06b266f1f35a7cccde847352a599df59d2
Kir Neboula <newbie13xd@outlook.com>
2018-08-07 09:21:41 +0200

    Removed unnecessary function call.

--- js/script3.js
+++ js/script3.js
@@ -393,6 +393,5 @@ if (localStorage.getItem("scheduleFiletype") !== "png" || localStorage.getItem("
     localStorage.setItem("scheduleFiletype", "png");
 }
 
-checkOrientation();
 document.getElementById("splashScreen").style.display = "none";
 if (location.protocol !== "https:") console.log("[Berzan.js] Jag skulle uppskatta om ni uppgraderade till HTTPS. Kan inte registrera service-workers annars. https://letsencrypt.org/getting-started/ :^)");
