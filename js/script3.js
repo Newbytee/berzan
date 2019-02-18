6f8a0a3dc6a617124babb27ddb85249e97257162
Newbyte <stehan253@edu.linkoping.se>
2019-02-18 14:32:57 +0000

    Moved more functions to their own files

--- js/script3.js
+++ js/script3.js
@@ -195,16 +195,9 @@ function checkOrientation() {
 }
 
 function showSnackbar(text) {
-    const SNACKBAR = document.getElementById("snackbar");
-    if (SNACKBAR.className.includes("show")) {
-        SNACKBAR.textContent = text;
-        return;
-    }
-    SNACKBAR.textContent = text;
-    SNACKBAR.className = "show";
-    setTimeout(function() {
-        SNACKBAR.className = SNACKBAR.className.replace("show", "");
-    }, 3000);
+    MODULES.load("utils", function() {
+        showSnackbar(text);
+    });
 }
 
 function updateServiceWorker() {
@@ -294,12 +287,9 @@ function loadHTML(URL) {
 }
 
 function createExternalPageViewer(URL) {
-    const EXT_PAGE_IFRAME = document.createElement("IFRAME");
-    EXT_PAGE_IFRAME.src = URL;
-    EXT_PAGE_IFRAME.id = "contentIframe";
-    CONTENT_DIV.innerHTML = "";
-    CONTENT_DIV.appendChild(EXT_PAGE_IFRAME);
-    sessionStorage.setItem("currentView", "~external")
+    MODULES.load("utils", function() {
+        createExternalPageViewer(URL);
+    });
 }
 
 function setupSchedulePage() {
