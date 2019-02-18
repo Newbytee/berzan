7f911b658b80bc642021562835798fd4fa8293ac
Newbyte <stehan253@edu.linkoping.se>
2019-02-18 13:15:44 +0000

    Revert "Did a lot of work on implementing a system for dynamically loading scripts. Currently uses a very hacky implementation that I'm unsure about browser compatibility on, but it does seem to work in Chromium, Firefox, and Konqueror"
    This reverts commit f6a0a18f4fd13632310d1276280ef301ac743206.
    
    This was not meant to be pushed to gh-pages

--- js/script3.js
+++ js/script3.js
@@ -13,7 +13,6 @@ const PAGE_TITLE = document.getElementById("titleName");
 const SPLASH_SCREEN = document.getElementById("splashScreen");
 const DATE = new Date();
 const LOG = new Log();
-const MODULES = new ModuleManager();
 const LANGUAGES = [ "sv-se", "en-gb", "de-de", "fr-fr" ];
 let allowKeyNav = true;
 let scheduleInit = false;
@@ -44,26 +43,6 @@ function Log() {
     
     this.get = function() {
         return this.data;
-    };
-}
-
-function ModuleManager() {
-    this.modules = document.getElementById("modules");
-    
-    this.load = function(name, callback) {
-        const PATH = "js/" + name + ".js";
-        
-        for (let i = 0; i < this.modules.children.length; i++) {
-            console.log(i);
-            if (this.modules.children[i].src.endsWith(PATH))
-                return; // Module is already loaded, so return
-        }
-        
-        const SCRIPT = document.createElement("SCRIPT");
-        SCRIPT.onload = callback;
-        SCRIPT.src = PATH;
-    
-        this.modules.appendChild(SCRIPT);
     };
 }
 
@@ -499,14 +478,126 @@ function getScheduleURL(className, week, weekDay, language, filetype) {
 }
 
 function setupSettings() {
-    MODULES.load("settings", function() {
-        setupSettings();
+    const CHANGE_STARTPAGE_BUTTONS = document.getElementsByClassName("startPagePicker");
+    const CLASS_SAVE_FIELD = document.getElementById("defaultClass");
+    const CHANGE_FILETYPE_BUTTONS = document.getElementsByClassName("filetypePicker");
+    const CHANGE_SLIDEOUT_SIDE_BUTTONS = document.getElementsByClassName("slideoutSidePicker");
+    const CHANGE_LANGUAGE_SELECTION = document.getElementById("languageSelection");
+    const SERVICE_WORKER_SELECTION = document.getElementById("serviceWorkerSelection");
+    const STYLE_SELECTION = document.getElementById("styleSelection");
+    
+    addToggle(STYLE_SELECTION, "newDesign", updateStyle);
+    addToggle(SERVICE_WORKER_SELECTION, "serviceWorkerEnabled", updateServiceWorker);
+
+    for (let i = 0; i < LANGUAGES.length; i++) {
+        if (LANGUAGES[i] === localStorage.getItem("appLanguage")) CHANGE_LANGUAGE_SELECTION.selectedIndex = i;
+    }
+
+    CHANGE_LANGUAGE_SELECTION.addEventListener("change", function() {
+        localStorage.setItem("appLanguage", LANGUAGES[CHANGE_LANGUAGE_SELECTION.selectedIndex]);
+        if (LANGUAGES[CHANGE_LANGUAGE_SELECTION.selectedIndex] === "de-de") alert("Due to what seems to be a bug with Novasoftware (the provider of the schedule), when German is selected as language the schedule will frequently fail to load.");
+    });
+
+    for (let i = 0; i < CHANGE_STARTPAGE_BUTTONS.length; i++) {
+        CHANGE_STARTPAGE_BUTTONS[i].addEventListener("click", function() {
+            switch (i) {
+                case 0:
+                    localStorage.setItem("startPage", "schedule");
+                    showSnackbar("Startsida bytt till schema");
+                    break;
+                case 1:
+                    localStorage.setItem("startPage", "lunch");
+                    showSnackbar("Startsida bytt till lunch");
+                    break;
+                case 2:
+                    localStorage.setItem("startPage", "etc");
+                    showSnackbar("Startsida bytt till övrigt");
+                    break;
+                default:
+                    localStorage.setItem("startPage", "schedule");
+                    showSnackbar("Startsida bytt till schema");
+                    break;
+            }
+        });
+    }
+
+    for (let i = 0; i < CHANGE_FILETYPE_BUTTONS.length; i++) {
+        CHANGE_FILETYPE_BUTTONS[i].addEventListener("click", function() {
+            switch (i) {
+                case 0:
+                    localStorage.setItem("scheduleFiletype", "png");
+                    showSnackbar("Schemat laddas nu som PNG");
+                    break;
+                case 1:
+                    localStorage.setItem("scheduleFiletype", "gif");
+                    showSnackbar("Schemat laddas nu som GIF");
+                    break;
+            }
+        });
+    }
+
+    for (let i = 0; i < CHANGE_SLIDEOUT_SIDE_BUTTONS.length; i++) {
+        CHANGE_SLIDEOUT_SIDE_BUTTONS[i].addEventListener("click", function() {
+            switch (i) {
+                case 0:
+                    localStorage.setItem("slideoutSide", "left");
+                    slideout.destroy();
+                    createSlideout();
+                    showSnackbar("Mobilmenyn flyttad till vänster");
+                    break;
+                case 1:
+                    localStorage.setItem("slideoutSide", "right");
+                    slideout.destroy();
+                    createSlideout();
+                    showSnackbar("Mobilmenyn flyttad till höger");
+                    break;
+            }
+        });
+    }
+
+    function saveDefaultClass() {
+        if (CLASS_SAVE_FIELD.value.length > 0) {
+            localStorage.setItem("defaultClass", CLASS_SAVE_FIELD.value);
+            showSnackbar(CLASS_SAVE_FIELD.value + " sparad som standardklass");
+        } else {
+            localStorage.removeItem("defaultClass");
+            showSnackbar("Standardklass borttagen");
+        }
+    }
+
+    CLASS_SAVE_FIELD.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            saveDefaultClass();
+        }
+    });
+
+    document.getElementById("saveButtonThingy").addEventListener("click", function() {
+        saveDefaultClass();
+    });
+
+    document.getElementById("resetButton").addEventListener("click", function() {
+        resetPreferences();
     });
 }
 
 function setupDebugMenu() {
-    MODULES.load("debugMenu", function() {
-        setupDebugMenu();
+    const SET_ITEM_INPUT = document.getElementsByClassName("setItemInput");
+    const SET_ITEM_BUTTON = document.getElementById("setItemButton");
+    const GET_ITEM_INPUT = document.getElementById("getItemInput");
+    const GET_ITEM_BUTTON = document.getElementById("getItemButton");
+    const LOAD_URL_INPUT = document.getElementById("loadURLInput");
+    const LOAD_URL_BUTTON = document.getElementById("loadURLButton");
+
+    SET_ITEM_BUTTON.addEventListener("click", function() {
+        localStorage.setItem(SET_ITEM_INPUT[0].value, SET_ITEM_INPUT[1].value);
+    });
+
+    GET_ITEM_BUTTON.addEventListener("click", function() {
+        showSnackbar(localStorage.getItem(GET_ITEM_INPUT.value));
+    });
+
+    LOAD_URL_BUTTON.addEventListener("click", function() {
+        loadPage(LOAD_URL_INPUT.value);
     });
 }
 
