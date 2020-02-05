c116c973e0f4fd5a5c7d479adb95ace64fdc630c
Newbyte <newbie13xd@gmail.com>
2020-02-05 16:46:57 +0100

    Always cache Slideout.js

--- sw.js
+++ sw.js
@@ -12,6 +12,7 @@ const URLS_TO_CACHE = [
     "/views/settings.html",
     "/manifest.json",
     "/img/logo/logo-512.png",
+    "https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.js"
 ];
 
 self.addEventListener("install", function(evnt) {
