9e71cf93d2692e39022ba98b283afc9dbd5b915c
Newbyte <newbie13xd@gmail.com>
2020-12-18 12:02:15 +0100

    Bump Berzan.js app cache version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const APP_CACHE = "berzanjs-app-cache-v1.2.1a";
+const APP_CACHE = "berzanjs-app-cache-v1.2.3";
 const CONTENT_CACHE = "berzanjs-schedule-cache-v1";
 const URLS_TO_CACHE = [
 	"/",
