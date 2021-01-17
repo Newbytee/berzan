12ef24c1e324872e6ba46fe3b2fb691c93c5eac7
Newbyte <newbie13xd@gmail.com>
2021-01-17 20:54:23 +0100

    Bump app cache version to 1.2.4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const APP_CACHE = "berzanjs-app-cache-v1.2.3";
+const APP_CACHE = "berzanjs-app-cache-v1.2.4";
 const CONTENT_CACHE = "berzanjs-schedule-cache-v1";
 const URLS_TO_CACHE = [
 	"/",
